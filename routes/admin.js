7394c0bc55c49d909880e6f3cc6397a1dca59dd2
Boris Tomas <boris.tomas@gmail.com>
2023-01-10 13:57:29 +0100

    jade draft

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'User' });
+  res.render('admin', { title: 'Admin' });
 });
 
 module.exports = router;
