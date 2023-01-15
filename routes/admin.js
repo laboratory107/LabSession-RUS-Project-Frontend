4fe94e3453f16e1698f7c987ca009eaf68bc4827
Lovro Posarić <lovro.posaric@gmail.com>
2023-01-15 19:21:46 +0100

    Users page

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const config = require("../config.json");
+const axios = require("axios");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'Admin' });
+router.get('/', function (req, res, next) {
+  const deviceId = 1;
+
+  axios.get(config.API_URL + "temp" + deviceId)
+    .then(resp => res.render('admin', { data: resp.data.result }))
+    .catch(err => res.render('admin', { msg: "Backend error" }));
+
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const config = require("../config.json");
+const axios = require("axios");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users', { title: 'User' });
+router.get('/', async function(req, res, next) {
+  const deviceId = 1;
+
+  try {
+    const resMeasurements = await axios.get(config.API_URL + "measurements/" + deviceId);
+    const resSentiment = await axios.get(config.API_URL + "sentiment/all/" + deviceId);
+    console.log(resSentiment);
+    res.render('users', { measurements: resMeasurements.data.result, sentiment: resSentiment.data.result});
+  }
+  catch (err) {
+    res.render('users', { msg: "Backend error" });
+  }
 });
 
 module.exports = router;
