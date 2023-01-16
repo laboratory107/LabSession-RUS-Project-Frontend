d05bab1f63085a3a2156136d1e6b958a5a577eea
Lovro Posarić <lovro.posaric@gmail.com>
2023-01-17 00:17:13 +0100

    Frontend main stuff

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,45 @@ var router = express.Router();
 const config = require("../config.json");
 const axios = require("axios");
 
-router.get('/', function (req, res, next) {
-  const deviceId = 1;
+/*
+{
+  "result": [
+    {
+      "DeviceID": 1,
+      "DeviceName": "Device001",
+      "RoomDescription": "First"
+    },
+    {
+      "DeviceID": 2,
+      "DeviceName": "Device002",
+      "RoomDescription": "First"
+    }
+  ]
+}
+*/
 
-  axios.get(config.API_URL + "temp" + deviceId)
-    .then(resp => res.render('admin', { data: resp.data.result }))
-    .catch(err => res.render('admin', { msg: "Backend error" }));
+const getAllData = async (deviceId) => {
+  const sentimentData = await axios.get(config.API_URL + "sentiment/avg/" + deviceId);
+  return sentimentData.data.result;
+};
 
+router.get('/', async function (req, res, next) {
+    try {
+      const devicesData = await axios.get(config.API_URL + "devices");
+      const devices = devicesData.data.result;
+
+      const promises = devices.map(async (device) => {
+        const sentimentData = await getAllData(device.DeviceID);
+        return sentimentData[0];
+      });
+      
+      const sentiments = await Promise.all(promises);
+
+      res.render('admin', { devices: devices, sentiment: sentiments});
+    }
+    catch (err) {
+      res.render('admin', { msg: "Backend error" });
+    }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ var router = express.Router();
 const config = require("../config.json");
 const axios = require("axios");
 
-router.get('/', async function(req, res, next) {
-  const deviceId = 1;
+router.get('/:id', async function(req, res, next) {
+  const deviceId = req.params.id;
 
   try {
     const resMeasurements = await axios.get(config.API_URL + "measurements/" + deviceId);
     const resSentiment = await axios.get(config.API_URL + "sentiment/all/" + deviceId);
-    console.log(resSentiment);
+
     res.render('users', { measurements: resMeasurements.data.result, sentiment: resSentiment.data.result});
   }
   catch (err) {
